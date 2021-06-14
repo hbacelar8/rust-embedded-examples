0b36ee783d6f013cfe5446e837695f3db401edf3
Henrique Bacelar <bacelarhenrique8@gmail.com>
2021-06-14 14:25:15 -0300

    Update rtic-serial code

--- examples/rtic-serial/src/main.rs
+++ examples/rtic-serial/src/main.rs
@@ -6,14 +6,19 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-
 use embedded_hal::digital::v2::OutputPin;
 use hal::{
-    gpio::gpioa::Parts,
+    afio, flash,
+    gpio::{
+        gpioa::{self, PA2, PA3, PA5},
+        Alternate, Floating, Input, Output, PushPull,
+    },
+    pac::USART2,
+    prelude::*,
+    rcc::Rcc,
     serial::{Config, Event, Serial, StopBits},
 };
-use hal::{gpio::*, pac, prelude::*};
+use panic_halt as _;
 use rtic::app;
 use stm32f1xx_hal as hal;
 
@@ -21,29 +26,28 @@ use stm32f1xx_hal as hal;
 const APP: () = {
     // Late Resources initialized at runtime after the init function
     struct Resources {
-        serial_handler:
-            Serial<pac::USART2, (gpioa::PA2<Alternate<PushPull>>, gpioa::PA3<Input<Floating>>)>,
-        led: gpioa::PA5<Output<PushPull>>,
+        SERIAL: Serial<USART2, (PA2<Alternate<PushPull>>, PA3<Input<Floating>>)>,
+        LED: PA5<Output<PushPull>>,
     }
 
     #[init]
     fn init(cx: init::Context) -> init::LateResources {
         // Set up peripherals
-        let mut rcc = cx.device.RCC.constrain();
-        let mut flash = cx.device.FLASH.constrain();
-        let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
-        let mut gpioa: Parts = cx.device.GPIOA.split(&mut rcc.apb2);
+        let mut rcc: Rcc = cx.device.RCC.constrain();
+        let mut flash: flash::Parts = cx.device.FLASH.constrain();
+        let mut afio: afio::Parts = cx.device.AFIO.constrain(&mut rcc.apb2);
+        let mut gpioa: gpioa::Parts = cx.device.GPIOA.split(&mut rcc.apb2);
 
         // Freeze clocks
         let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-        // Set up LED and UART2 pins
+        // Set up LED and USART2 pins
         let led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
         let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
         let rx = gpioa.pa3;
 
-        // Get UART2 instance
-        let mut serial_handler = Serial::usart2(
+        // Get USART2 instance
+        let mut serial = Serial::usart2(
             cx.device.USART2,
             (tx, rx),
             &mut afio.mapr,
@@ -56,12 +60,12 @@ const APP: () = {
         );
 
         // Enable RX interruption
-        serial_handler.listen(Event::Rxne);
+        serial.listen(Event::Rxne);
 
         // Set up late resources
         init::LateResources {
-            serial_handler,
-            led,
+            SERIAL: serial,
+            LED: led,
         }
     }
 
@@ -72,10 +76,11 @@ const APP: () = {
     }
 
     /// USART2 ISR
-    #[task(binds = USART2, resources = [serial_handler, led])]
-    fn USART2(c: USART2::Context) {
-        let serial = c.resources.serial_handler;
-        let led = c.resources.led;
+    #[task(binds = USART2, resources = [SERIAL, LED])]
+    fn usart2_isr(c: usart2_isr::Context) {
+        let serial: &mut Serial<USART2, (PA2<Alternate<PushPull>>, PA3<Input<Floating>>)> =
+            c.resources.SERIAL;
+        let led: &mut PA5<Output<PushPull>> = c.resources.LED;
 
         // Read serial data register, automatic clearing RX interruption flag
         let received = serial.read().unwrap();
