eefc436908ee4b96e6afa38db2c961012e563d6f
Henrique Bacelar <bacelarhenrique8@gmail.com>
2021-06-15 19:05:58 -0300

    Major update on rtic example

--- examples/rtic/src/main.rs
+++ examples/rtic/src/main.rs
@@ -6,37 +6,43 @@
 
 mod lcd;
 
+use crate::lcd::LCD;
 use core::mem::MaybeUninit;
 use embedded_hal::digital::v2::OutputPin;
-use heapless::{self, Vec};
-use lcd::LCD;
-use panic_halt as _;
-
-use hal::{
+use heapless::{consts, Vec};
+use rtic::app;
+use stm32f1xx_hal::{
+    self, afio,
     delay::Delay,
+    flash,
     gpio::{
         gpioa::{self, PA2, PA3, PA5},
         gpiob::{self, PB6, PB7, PB8},
-        gpioc::{self},
-        Alternate, Floating, Input, Output, PushPull, State,
+        gpioc, Alternate, Floating, Input, Output, PushPull, State,
     },
-    pac::{self, TIM1, TIM4},
+    pac::{TIM1, TIM4, USART2},
     prelude::*,
     pwm::{Channel, Pwm, C1, C2, C3},
-    serial::{Config, Event, Serial, StopBits},
-    timer::{CountDownTimer, Tim4NoRemap, Timer},
+    rcc::Rcc,
+    serial::{self, Config, Serial, StopBits},
+    timer::{self, CountDownTimer, Tim4NoRemap, Timer},
 };
-use rtic::app;
-use stm32f1xx_hal as hal;
+
+pub struct SerialStruct {
+    counter: u8,
+    app: u8,
+    cmd: u8,
+    len: u8,
+    data: Vec<u8, consts::U32>,
+}
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     // Late Resources initialized at runtime after the init function
     struct Resources {
-        serial_handler:
-            Serial<pac::USART2, (gpioa::PA2<Alternate<PushPull>>, gpioa::PA3<Input<Floating>>)>,
-        timer_handler: CountDownTimer<TIM1>,
-        pwm_handler: Pwm<
+        LED: PA5<Output<PushPull>>,
+        TIMER: CountDownTimer<TIM1>,
+        PWM: Pwm<
             TIM4,
             Tim4NoRemap,
             (C1, C2, C3),
@@ -46,29 +52,21 @@ const APP: () = {
                 PB8<Alternate<PushPull>>,
             ),
         >,
-
-        lcd: &'static mut LCD,
-        led: gpioa::PA5<Output<PushPull>>,
-
-        rx_buffer: Vec<u8, heapless::consts::U32>,
-
-        #[init(0)]
-        byte_counter: u8,
-
-        #[init(false)]
-        led_state: bool,
+        LCD: &'static mut LCD,
+        SERIAL: Serial<USART2, (PA2<Alternate<PushPull>>, PA3<Input<Floating>>)>,
+        SERIAL_STRUCT: SerialStruct,
 
         #[init(1)]
-        led_freq: u8,
+        LED_FREQ: u8,
     }
 
     /// Initialization task
     #[init]
     fn init(cx: init::Context) -> init::LateResources {
         // Set up peripherals
-        let mut rcc = cx.device.RCC.constrain();
-        let mut flash = cx.device.FLASH.constrain();
-        let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
+        let mut rcc: Rcc = cx.device.RCC.constrain();
+        let mut flash: flash::Parts = cx.device.FLASH.constrain();
+        let mut afio: afio::Parts = cx.device.AFIO.constrain(&mut rcc.apb2);
         let mut gpioa: gpioa::Parts = cx.device.GPIOA.split(&mut rcc.apb2);
         let mut gpiob: gpiob::Parts = cx.device.GPIOB.split(&mut rcc.apb2);
         let mut gpioc: gpioc::Parts = cx.device.GPIOC.split(&mut rcc.apb2);
@@ -76,22 +74,19 @@ const APP: () = {
         // Freeze clocks
         let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-        // Get delay instance
-        let delay = Delay::new(cx.core.SYST, clocks);
-
         // Configure board LED
-        let led: PA5<Output<PushPull>> = gpioa
+        let led = gpioa
             .pa5
             .into_push_pull_output_with_state(&mut gpioa.crl, State::High);
 
-        // Configure timer TIM1
+        // Configure timer
         let mut timer =
             Timer::tim1(cx.device.TIM1, &clocks, &mut rcc.apb2).start_count_down(1.hz());
 
         // Configure PWM
-        let c1: PB6<Alternate<PushPull>> = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
-        let c2: PB7<Alternate<PushPull>> = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
-        let c3: PB8<Alternate<PushPull>> = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
+        let c1 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+        let c2 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
+        let c3 = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
         let pwm_pins = (c1, c2, c3);
 
         let mut pwm = Timer::tim4(cx.device.TIM4, &clocks, &mut rcc.apb1)
@@ -100,6 +95,9 @@ const APP: () = {
         pwm.enable(Channel::C1);
         pwm.enable(Channel::C2);
         pwm.enable(Channel::C3);
+
+        // Get delay instance
+        let delay = Delay::new(cx.core.SYST, clocks);
 
         // Configure LCD
         let rs = gpioa.pa0.into_push_pull_output(&mut gpioa.crl);
@@ -112,16 +110,18 @@ const APP: () = {
         let lcd = unsafe {
             static mut LCD: MaybeUninit<LCD> = MaybeUninit::uninit();
 
+            // Write directly into the static storage
             LCD.as_mut_ptr()
                 .write(LCD::new(rs, en, d4, d5, d6, d7, delay));
+
             &mut *LCD.as_mut_ptr()
         };
 
         lcd.init();
 
         // Configure UART2
-        let tx: PA2<Alternate<PushPull>> = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
-        let rx: PA3<Input<Floating>> = gpioa.pa3;
+        let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+        let rx = gpioa.pa3;
 
         let mut serial = Serial::usart2(
             cx.device.USART2,
@@ -135,76 +135,102 @@ const APP: () = {
             &mut rcc.apb1,
         );
 
-        // Create RX buffer
-        let rx_buffer: Vec<u8, heapless::consts::U32> = Vec::new();
-
         // Enable RX interruption
-        serial.listen(Event::Rxne);
+        serial.listen(serial::Event::Rxne);
 
-        // Enable timer coutdown interruption
-        timer.listen(hal::timer::Event::Update);
+        // Enable timer countdown interruption
+        timer.listen(timer::Event::Update);
 
-        // Set late resources
+        // Initialize serial struct
+        let serial_struct = SerialStruct {
+            counter: 0,
+            app: 0,
+            cmd: 0,
+            len: 0,
+            data: Vec::new(),
+        };
+
+        // Assign late resources
         init::LateResources {
-            serial_handler: serial,
-            timer_handler: timer,
-            pwm_handler: pwm,
-            lcd,
-            led,
-            rx_buffer,
+            LED: led,
+            TIMER: timer,
+            PWM: pwm,
+            LCD: lcd,
+            SERIAL: serial,
+            SERIAL_STRUCT: serial_struct,
         }
     }
 
     /// Idle task
     #[idle]
     fn idle(_: idle::Context) -> ! {
-        // Infinite loop so system doesn't go to sleep
         loop {}
     }
 
     /// TIM1 ISR
-    #[task(binds = TIM1_UP, priority = 1, resources = [timer_handler, led, led_state])]
-    fn TIM1_UP(cx: TIM1_UP::Context) {
-        if *cx.resources.led_state {
-            cx.resources.led.set_high().ok();
-            *cx.resources.led_state = false;
-        } else {
-            cx.resources.led.set_low().ok();
-            *cx.resources.led_state = true;
-        }
+    #[task(binds=TIM1_UP, resources=[TIMER, LED])]
+    fn tim1_up_isr(mut cx: tim1_up_isr::Context) {
+        // Clear TIM1 update interrupt flag
+        cx.resources.TIMER.lock(|timer| {
+            timer.clear_update_interrupt_flag();
+        });
 
-        cx.resources.timer_handler.clear_update_interrupt_flag();
-        // cx.spawn.uart_handler2().unwrap();
+        // Toggle LED
+        cx.resources.LED.lock(|led| {
+            led.toggle().ok();
+        });
     }
 
-    /// UART2 ISR
-    #[task(binds = USART2, spawn = [uart_handler], resources = [serial_handler, byte_counter, rx_buffer])]
-    fn USART2(c: USART2::Context) {
-        // Read serial data register, automatic clearing RX interruption flag
-        let received = c.resources.serial_handler.read().unwrap();
-        let rx_buffer: &mut Vec<u8, heapless::consts::U32> = c.resources.rx_buffer;
+    /// USART2 ISR
+    #[task(binds=USART2, priority=3, spawn=[msg_handler], resources=[SERIAL, SERIAL_STRUCT])]
+    fn usart2_isr(cx: usart2_isr::Context) {
+        // Get local access to shared resources
+        let serial: &mut Serial<USART2, (PA2<Alternate<PushPull>>, PA3<Input<Floating>>)> =
+            cx.resources.SERIAL;
+        let serial_struct: &mut SerialStruct = cx.resources.SERIAL_STRUCT;
 
-        if *c.resources.byte_counter < 3 {
-            rx_buffer.push(received).unwrap();
-            *c.resources.byte_counter += 1;
-        } else {
-            if *c.resources.byte_counter == rx_buffer[2] + 2 {
-                *c.resources.byte_counter = 0;
-                rx_buffer.push(received).unwrap();
-                c.spawn.uart_handler().unwrap();
-            } else {
-                rx_buffer.push(received).unwrap();
-                *c.resources.byte_counter += 1;
+        // Read received byte, automatic clearing RX interruption flag
+        let byte_received = serial.read().unwrap();
+
+        match serial_struct.counter {
+            0 => {
+                if byte_received == 0xA0 || byte_received == 0xB0 || byte_received == 0xC0 {
+                    serial_struct.app = byte_received;
+                    serial_struct.counter += 1;
+                }
+            }
+            1 => {
+                serial_struct.cmd = byte_received;
+                serial_struct.counter += 1;
+            }
+            2 => {
+                serial_struct.len = byte_received;
+                serial_struct.counter += 1;
+
+                if serial_struct.len == 0 {
+                    serial_struct.counter = 0;
+                    cx.spawn.msg_handler().unwrap();
+                }
+            }
+            _ => {
+                serial_struct.data.push(byte_received).ok();
+                serial_struct.counter += 1;
+
+                if serial_struct.counter == serial_struct.len + 3 {
+                    serial_struct.counter = 0;
+                    cx.spawn.msg_handler().unwrap();
+                }
             }
         }
     }
 
-    /// Handle commands received through UART
-    #[task(resources = [lcd, led, timer_handler, pwm_handler, rx_buffer, led_freq])]
-    fn uart_handler(c: uart_handler::Context) {
-        let rx_buffer: &mut Vec<u8, heapless::consts::U32> = c.resources.rx_buffer;
-        let timer_handler: &mut CountDownTimer<TIM1> = c.resources.timer_handler;
-        let pwm_handler: &mut Pwm<
+    /// Message handler callback function
+    #[task(priority=2, resources=[LED, TIMER, PWM, LCD, SERIAL_STRUCT, LED_FREQ])]
+    fn msg_handler(mut cx: msg_handler::Context) {
+        // Get local access to shared resources
+        let led: &mut PA5<Output<PushPull>> = cx.resources.LED;
+        let timer: &mut CountDownTimer<TIM1> = cx.resources.TIMER;
+        let pwm: &mut Pwm<
             TIM4,
             Tim4NoRemap,
             (C1, C2, C3),
@@ -213,86 +239,111 @@ const APP: () = {
                 PB7<Alternate<PushPull>>,
                 PB8<Alternate<PushPull>>,
             ),
-        > = c.resources.pwm_handler;
-        let lcd: &mut LCD = c.resources.lcd;
-        let app = rx_buffer[0];
-        let cmd = rx_buffer[1];
-        let len = rx_buffer[2];
+        > = cx.resources.PWM;
+        let lcd: &mut LCD = cx.resources.LCD;
+        let led_freq: &mut u8 = cx.resources.LED_FREQ;
 
-        match app {
-            0xC0 => match cmd {
-                0x01 => {
-                    if rx_buffer[3] == 1 {
-                        pwm_handler.set_duty(Channel::C1, 0);
-                        pwm_handler.set_duty(Channel::C2, 0);
-                        pwm_handler.set_duty(Channel::C3, 0);
-                    } else if rx_buffer[3] == 2 {
-                        let max_duty_cycle = pwm_handler.get_max_duty();
-                        pwm_handler.set_duty(Channel::C1, max_duty_cycle);
-                        pwm_handler.set_duty(Channel::C2, max_duty_cycle);
-                        pwm_handler.set_duty(Channel::C3, max_duty_cycle);
+        cx.resources
+            .SERIAL_STRUCT
+            .lock(|serial_struct: &mut SerialStruct| {
+                match serial_struct.app {
+                    // RGB LED commands
+                    0xA0 => {
+                        // Get max duty cycle and divide it by steps of 255 for the color range
+                        let step = pwm.get_max_duty() / 255;
+
+                        match serial_struct.cmd {
+                            0x00 => {
+                                // Set 3 colors intensities
+
+                                let red = serial_struct.data[0];
+                                let green = serial_struct.data[1];
+                                let blue = serial_struct.data[2];
+
+                                pwm.set_duty(Channel::C1, step * red as u16);
+                                pwm.set_duty(Channel::C2, step * green as u16);
+                                pwm.set_duty(Channel::C3, step * blue as u16);
+                            }
+                            0x01 => {
+                                // Set red color intensity
+
+                                let red = serial_struct.data[0];
+
+                                pwm.set_duty(Channel::C1, step * red as u16);
+                            }
+                            0x02 => {
+                                // Set green color intensity
+
+                                let green = serial_struct.data[0];
+
+                                pwm.set_duty(Channel::C2, step * green as u16);
+                            }
+                            0x03 => {
+                                // Set blue color intensity
+
+                                let blue = serial_struct.data[0];
+
+                                pwm.set_duty(Channel::C3, step * blue as u16);
+                            }
+                            _ => {}
+                        }
                     }
-                }
-                0x02 => {
-                    let step = pwm_handler.get_max_duty() / 255;
-                    let red_val = rx_buffer[3];
-                    pwm_handler.set_duty(Channel::C1, step * (red_val as u16));
-                }
-                0x03 => {
-                    let step = pwm_handler.get_max_duty() / 255;
-                    let green_val = rx_buffer[3];
-                    pwm_handler.set_duty(Channel::C2, step * (green_val as u16));
-                }
-                0x04 => {
-                    let step = pwm_handler.get_max_duty() / 255;
-                    let blue_val = rx_buffer[3];
-                    pwm_handler.set_duty(Channel::C3, step * (blue_val as u16));
-                }
-                _ => (),
-            },
-            0xB0 => match cmd {
-                0x01 => {
-                    let new_freq = rx_buffer[3];
-                    timer_handler.start((new_freq as u32).hz());
-                    *c.resources.led_freq = new_freq;
+                    // Board LED commands
+                    0xB0 => match serial_struct.cmd {
+                        0x01 => {
+                            // Set new LED blink frequency
 
-                    // Enable timer coutdown interruption
-                    timer_handler.listen(hal::timer::Event::Update);
-                }
-                0x02 => {
-                    // Unable timer coutdown interruption
-                    timer_handler.unlisten(hal::timer::Event::Update);
-                    c.resources.led.set_low().ok();
-                }
-                0x03 => {
-                    // Enable timer coutdown interruption
-                    timer_handler.listen(hal::timer::Event::Update);
-                }
-                _ => (),
-            },
-            0xA0 => match cmd {
-                0x01 => lcd.send_cmd(rx_buffer[3]),
-                0x02 => {
-                    for n in 3..(3 + len) {
-                        lcd.send_data(rx_buffer[n as usize]);
-                    }
-                }
-                _ => (),
-            },
-            _ => (),
-        }
+                            let new_led_freq = serial_struct.data[0];
 
-        rx_buffer.clear();
+                            timer.start((new_led_freq as u32).hz());
+                            *led_freq = new_led_freq;
+
+                            // Enable timer countdown interruption
+                            timer.listen(timer::Event::Update);
+                        }
+                        0x02 => {
+                            // Turn LED off
+
+                            // Unable timer countdown interruption
+                            timer.unlisten(timer::Event::Update);
+                            led.set_low().ok();
+                        }
+                        0x03 => {
+                            // Turn LED on
+
+                            // Enable timer countdown interruption
+                            timer.listen(timer::Event::Update);
+                        }
+                        _ => {}
+                    },
+                    // LCD commands
+                    0xC0 => match serial_struct.cmd {
+                        0x01 => {
+                            // Send command to LCD
+
+                            let cmd = serial_struct.data[0];
+
+                            lcd.send_cmd(cmd);
+                        }
+                        0x02 => {
+                            // Send data to LCD
+
+                            for n in 0..(serial_struct.len) {
+                                let data = serial_struct.data[n as usize];
+
+                                lcd.send_data(data);
+                            }
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+
+                serial_struct.data.clear();
+            });
     }
-
-    // /// Handle commands received through UART
-    // #[task(priority = 2)]
-    // fn uart_handler2(_: uart_handler2::Context) {
-    //     let _i= 0;
-    // }
 
     extern "C" {
         fn TAMPER();
-    // fn EXTI1();
     }
 };
